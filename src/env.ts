99253546579585f5ade100c70d393a55992683fe
gabinaureche <hello@gabinaureche.com>
2021-04-13 17:27:38 +0200

    Add integration tests

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 import path from "path";
 
-if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "development") {
   require("dotenv-expand")(
     require("dotenv").config({ path: path.join(__dirname, "..", ".env") })
   );
