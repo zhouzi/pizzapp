256b4e26590fde8b2a58ebc0e638461e377a85b7
gabinaureche <hello@gabinaureche.com>
2021-04-13 17:40:35 +0200

    Split app

--- src/api/__tests__/pizzas.ts
+++ src/api/__tests__/pizzas.ts
@@ -1,7 +1,6 @@
-import express from "express";
 import supertest from "supertest";
 import { database } from "../../database";
-import { router } from "../pizzas";
+import { app } from "../../app";
 
 describe("/api/pizzas", () => {
   beforeAll(async () => {
@@ -13,8 +12,8 @@ describe("/api/pizzas", () => {
   });
 
   it("should return pizzas", async () => {
-    const request = supertest(express().use(router));
-    const response = await request.get("/pizzas");
+    const request = supertest(app);
+    const response = await request.get("/api/pizzas");
 
     expect(response.status).toBe(200);
   });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-import express from "express";
 import "./env";
-import { router } from "./api";
-
-const app = express();
-
-app.use(express.json());
-app.use(router);
-
-app.get("/", (req, res) => {
-  res.redirect("/api/pizzas");
-});
+import { app } from "./app";
 
 app.listen(process.env.PORT);
